bd80745d0f7d6cf6bb2d1b7288997fff1cdf52fe
QuangPV10 <phamvanquang.work@gmail.com>
2023-10-02 22:55:59 +0700

    set up

---
+++ src/components/Layout/HeaderOnly/index.js
@@ -0,0 +1,11 @@
+import Header from './Header';
+
+function DefaultLayout({ children }) {
+    return (
+        <div>
+            <Header />
+        </div>
+    );
+}
+
+export default DefaultLayout;
